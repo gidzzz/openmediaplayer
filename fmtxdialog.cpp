70b244e15b6f6ff7aaa50ab2a408515892121e92
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2010-12-24 01:29:15 +0200

    Added FMTX dialog

---
+++ fmtxdialog.cpp
@@ -0,0 +1,36 @@
+#include "fmtxdialog.h"
+#include "ui_fmtxdialog.h"
+
+FMTXDialog::FMTXDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::FMTXDialog)
+{
+    ui->setupUi(this);
+    setAttribute(Qt::WA_DeleteOnClose);
+    connect(ui->saveButton, SIGNAL(accepted()), this, SLOT(onSaveClicked()));
+#ifdef Q_WS_MAEMO_5
+    fmtxList = new QMaemo5ValueButton(tr("Frequency"), this);
+    fmtxList->setValueText("100.00 MHz");
+    fmtxList->setValueLayout(QMaemo5ValueButton::ValueBesideText);
+    ui->fmtxLayout->addWidget(fmtxList);
+
+    fmtxSelector = new QMaemo5ListPickSelector(this);
+    fmtxItems = new QListView(this);
+    //fmtxSelector->setModel(&fmtxItems);
+    fmtxList->setPickSelector(fmtxSelector);
+#endif
+}
+
+FMTXDialog::~FMTXDialog()
+{
+    delete ui;
+}
+
+void FMTXDialog::onSaveClicked()
+{
+    if(ui->fmtxCheckbox->isChecked())
+        system("fmtx_client -p 1");
+    else
+        system("fmtx_client -p 0");
+    this->close();
+}
---
+++ fmtxdialog.h
@@ -0,0 +1,38 @@
+#ifndef FMTXDIALOG_H
+#define FMTXDIALOG_H
+
+#include <QDialog>
+#ifdef Q_WS_MAEMO_5
+#include <QMaemo5ListPickSelector>
+#include <QMaemo5InformationBox>
+#include <QMaemo5ValueButton>
+#include <QAbstractListModel>
+#include <QList>
+#include <QListView>
+#endif
+
+namespace Ui {
+    class FMTXDialog;
+}
+
+class FMTXDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit FMTXDialog(QWidget *parent = 0);
+    ~FMTXDialog();
+
+private:
+    Ui::FMTXDialog *ui;
+#ifdef Q_WS_MAEMO_5
+    QMaemo5ValueButton *fmtxList;
+    QMaemo5ListPickSelector *fmtxSelector;
+    QListView *fmtxItems;
+#endif
+
+private slots:
+    void onSaveClicked();
+};
+
+#endif // FMTXDIALOG_H
