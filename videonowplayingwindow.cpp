5c0666b71fdfa549c3a56e2ba4d6985a405da8fd
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2010-12-27 22:24:05 +0200

    Added a UI for video playback.

---
+++ videonowplayingwindow.cpp
@@ -0,0 +1,84 @@
+#include "videonowplayingwindow.h"
+#include "ui_videonowplayingwindow.h"
+
+VideoNowPlayingWindow::VideoNowPlayingWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::VideoNowPlayingWindow)
+{
+    ui->setupUi(this);
+#ifdef Q_WS_MAEMO_5
+    setAttribute(Qt::WA_Maemo5StackedWindow);
+#endif
+    setAttribute(Qt::WA_DeleteOnClose);
+    volumeTimer = new QTimer(this);
+    volumeTimer->setInterval(3000);
+    this->setIcons();
+    this->connectSignals();
+    ui->volumeSlider->hide();
+}
+
+VideoNowPlayingWindow::~VideoNowPlayingWindow()
+{
+    delete ui;
+}
+
+void VideoNowPlayingWindow::setIcons()
+{
+    ui->wmCloseButton->setIcon(QIcon(wmCloseIcon));
+    ui->prevButton->setIcon(QIcon(prevButtonIcon));
+    ui->playButton->setIcon(QIcon(playButtonIcon));
+    ui->nextButton->setIcon(QIcon(nextButtonIcon));
+    ui->deleteButton->setIcon(QIcon(deleteButtonIcon));
+    ui->shareButton->setIcon(QIcon(shareButtonIcon));
+    ui->volumeButton->setIcon(QIcon(volumeButtonIcon));
+}
+
+void VideoNowPlayingWindow::connectSignals()
+{
+    connect(ui->volumeButton, SIGNAL(clicked()), this, SLOT(toggleVolumeSlider()));
+    connect(ui->volumeButton, SIGNAL(clicked()), this, SLOT(volumeWatcher()));
+    connect(volumeTimer, SIGNAL(timeout()), this, SLOT(toggleVolumeSlider()));
+    connect(ui->volumeSlider, SIGNAL(sliderPressed()), volumeTimer, SLOT(stop()));
+    connect(ui->volumeSlider, SIGNAL(sliderReleased()), volumeTimer, SLOT(start()));
+#ifdef Q_WS_MAEMO_5
+    QDBusConnection::sessionBus().connect("com.nokia.mafw.renderer.Mafw-Gst-Renderer-Plugin.gstrenderer",
+                                          "/com/nokia/mafw/renderer/gstrenderer",
+                                          "com.nokia.mafw.extension",
+                                          "property_changed",
+                                          this, SLOT(onVolumeChanged(const QDBusMessage &)));
+#endif
+}
+
+void VideoNowPlayingWindow::toggleVolumeSlider()
+{
+    if(ui->volumeSlider->isHidden()) {
+        ui->buttonWidget->hide();
+        ui->volumeSlider->show();
+    } else {
+        ui->volumeSlider->hide();
+        ui->buttonWidget->show();
+        if(volumeTimer->isActive())
+            volumeTimer->stop();
+    }
+}
+
+#ifdef Q_WS_MAEMO_5
+void VideoNowPlayingWindow::onVolumeChanged(const QDBusMessage &msg)
+{
+    /*dbus-send --print-reply --type=method_call --dest=com.nokia.mafw.renderer.Mafw-Gst-Renderer-Plugin.gstrenderer \
+                 /com/nokia/mafw/renderer/gstrenderer com.nokia.mafw.extension.get_extension_property string:volume*/
+    if (msg.arguments()[0].toString() == "volume") {
+        int volumeLevel = qdbus_cast<QVariant>(msg.arguments()[1]).toInt();
+#ifdef DEBUG
+        qDebug() << QString::number(volumeLevel);
+#endif
+        ui->volumeSlider->setValue(volumeLevel);
+    }
+}
+#endif
+
+void VideoNowPlayingWindow::volumeWatcher()
+{
+    if(!ui->volumeSlider->isHidden())
+        volumeTimer->start();
+}
---
+++ videonowplayingwindow.h
@@ -0,0 +1,44 @@
+#ifndef VIDEONOWPLAYINGWINDOW_H
+#define VIDEONOWPLAYINGWINDOW_H
+
+#include <QMainWindow>
+#include <QIcon>
+#include <QTimer>
+#include <QtDBus>
+
+#define wmCloseIcon "/etc/hildon/theme/images/wmBackIconPressed.png"
+#define prevButtonIcon "/etc/hildon/theme/mediaplayer/Back.png"
+#define playButtonIcon "/etc/hildon/theme/mediaplayer/Play.png"
+#define pauseButtonIcon "/etc/hildon/theme/mediaplayer/Pause.png"
+#define nextButtonIcon "/etc/hildon/theme/mediaplayer/Forward.png"
+#define shareButtonIcon "/usr/share/icons/hicolor/48x48/hildon/general_share.png"
+#define deleteButtonIcon "/usr/share/icons/hicolor/48x48/hildon/general_delete.png"
+#define volumeButtonIcon "/usr/share/icons/hicolor/64x64/hildon/mediaplayer_volume.png"
+
+namespace Ui {
+    class VideoNowPlayingWindow;
+}
+
+class VideoNowPlayingWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit VideoNowPlayingWindow(QWidget *parent = 0);
+    ~VideoNowPlayingWindow();
+
+private:
+    Ui::VideoNowPlayingWindow *ui;
+    void setIcons();
+    void connectSignals();
+    QTimer *volumeTimer;
+
+private slots:
+    void toggleVolumeSlider();
+    void volumeWatcher();
+#ifdef Q_WS_MAEMO_5
+    void onVolumeChanged(const QDBusMessage &msg);
+#endif
+};
+
+#endif // VIDEONOWPLAYINGWINDOW_H
