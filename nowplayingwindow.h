2efa8c0535deab27fe84f553258308c92b301ea8
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2011-01-05 10:09:11 +0200

    Fixed broken merge.

--- nowplayingwindow.h
+++ nowplayingwindow.h
@@ -6,10 +6,6 @@
 #include <QtDBus>
 #include <QTimer>
 
-#ifdef Q_WS_MAEMO_5
-    #include <QLibrary>
-#endif
-
 #include "mirror.h"
 #include "cqgraphicsview.h"
 #include "ui_nowplayingwindow.h"
@@ -22,39 +18,9 @@
 #else
     class MafwRendererAdapter;
 #endif
-
-#define prevButtonIcon "/etc/hildon/theme/mediaplayer/Back.png"
-#define playButtonIcon "/etc/hildon/theme/mediaplayer/Play.png"
-#define pauseButtonIcon "/etc/hildon/theme/mediaplayer/Pause.png"
-#define nextButtonIcon "/etc/hildon/theme/mediaplayer/Forward.png"
-#define repeatButtonIcon "/etc/hildon/theme/mediaplayer/Repeat.png"
-#define repeatButtonPressedIcon "/etc/hildon/theme/mediaplayer/RepeatPressed.png"
-#define shuffleButtonIcon "/etc/hildon/theme/mediaplayer/Shuffle.png"
-#define shuffleButtonPressed "/etc/hildon/theme/mediaplayer/ShufflePressed.png"
-#define volumeButtonIcon "/usr/share/icons/hicolor/64x64/hildon/mediaplayer_volume.png"
-#define albumImage "/usr/share/icons/hicolor/295x295/hildon/mediaplayer_default_album.png"
-
->>>>>>> 9b57e78182287e49ebf50e02567db609e73c4ae9
 namespace Ui {
     class NowPlayingWindow;
 }
-
-<<<<<<< HEAD
-class MafwRendererAdapter;
-
-=======
-enum npSongUserRoles { npUserRoleName=Qt::UserRole, npUserRoleSongName };
-
-class PlayListDelegate : public QStyledItemDelegate
-{
-public:
-        explicit PlayListDelegate(QObject *parent=0) : QStyledItemDelegate(parent) {}
-        virtual ~PlayListDelegate() {}
-
-        void paint (QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const;
-        QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const;
-};
->>>>>>> 9b57e78182287e49ebf50e02567db609e73c4ae9
 
 class NowPlayingWindow : public QMainWindow
 {
