1474c57195862916d672d9d0ca191b0ece515f60
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2011-03-13 18:22:22 +0200

    New settings dialog to configure action on exit

---
+++ settingsdialog.cpp
@@ -0,0 +1,25 @@
+#include "settingsdialog.h"
+#include "ui_settingsdialog.h"
+
+SettingsDialog::SettingsDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::SettingsDialog)
+{
+    ui->setupUi(this);
+    this->setAttribute(Qt::WA_DeleteOnClose);
+}
+
+SettingsDialog::~SettingsDialog()
+{
+    delete ui;
+}
+
+void SettingsDialog::accept()
+{
+    int index = ui->onExitBox->currentIndex();
+    if (index == 0)
+        QSettings().setValue("main/onApplicationExit", "do-nothing");
+    else if (index == 1)
+        QSettings().setValue("main/onApplicationExit", "stop-playback");
+    this->close();
+}
---
+++ settingsdialog.h
@@ -0,0 +1,26 @@
+#ifndef SETTINGSDIALOG_H
+#define SETTINGSDIALOG_H
+
+#include <QDialog>
+#include <QSettings>
+
+namespace Ui {
+    class SettingsDialog;
+}
+
+class SettingsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsDialog(QWidget *parent = 0);
+    ~SettingsDialog();
+
+private:
+    Ui::SettingsDialog *ui;
+
+private slots:
+    void accept();
+};
+
+#endif // SETTINGSDIALOG_H
