3535dc1804ae4e06945d93f48209ff622c85cd4d
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2011-04-24 19:01:42 +0300

    Fix coding style, get rid of undeed code

--- maemo5deviceevents.cpp
+++ maemo5deviceevents.cpp
@@ -40,21 +40,21 @@ void Maemo5DeviceEvents::onScreenLocked(QString state)
     qDebug() << "Maemo5DeviceEvents: Screen lock status changed: " << state;
 #endif
     this->screenState = state;
-    if(state == "unlocked")
+    if (state == "unlocked")
         emit screenLocked(false);
-    else if(state == "locked")
+    else if (state == "locked")
         emit screenLocked(true);
 }
 
 bool Maemo5DeviceEvents::isScreenLocked()
 {
-    if(!this->screenState.isNull()) {
-        if(this->screenState == "locked") {
+    if (!this->screenState.isNull()) {
+        if (this->screenState == "locked") {
 #ifdef DEBUG
             qDebug() << "Maemo5DeviceEvents: Screen is locked";
 #endif
             return true;
-        } else if(this->screenState == "unlocked") {
+        } else if (this->screenState == "unlocked") {
 #ifdef DEBUG
             qDebug() << "Maemo5DeviceEvents: Screen is unlocked";
 #endif
@@ -65,16 +65,15 @@ bool Maemo5DeviceEvents::isScreenLocked()
         // Read from sysfs
         QFile tsDisabled("/sys/devices/platform/omap2_mcspi.1/spi1.0/disable_ts");
         tsDisabled.open(QIODevice::ReadOnly);
-        QTextStream tsDisabledStream(&tsDisabled);
-        int tsState = tsDisabledStream.readLine().toInt();
+        int tsState = tsDisabled.readLine().toInt();
         tsDisabled.close();
-        if(tsState == 1) {
+        if (tsState == 1) {
             this->screenState = "locked";
 #ifdef DEBUG
             qDebug() << "Maemo5DeviceEvents: Screen is locked";
 #endif
             return true;
-        } else if(tsState == 0) {
+        } else if (tsState == 0) {
             this->screenState = "unlocked";
 #ifdef DEBUG
             qDebug() << "Maemo5DeviceEvents: Screen is unlocked";
