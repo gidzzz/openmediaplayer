ba4bedd74a005284749c65bfc7bdd5d141d08e52
Pali Rohár <pali.rohar@gmail.com>
2012-03-18 17:02:09 +0100

    Add support for Bluetooth Headset Stop button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -669,9 +669,11 @@ void MainWindow::onStateChanged(int state)
 void MainWindow::onBluetoothButtonPressed(QDBusMessage msg)
 {
     if (msg.arguments()[0] == "ButtonPressed") {
-        if (msg.arguments()[1] == "play-cd" || msg.arguments()[1] == "pause-cd") {
+        if (msg.arguments()[1] == "play-cd" || msg.arguments()[1] == "pause-cd")
             this->pausePlay();
-        } else if (msg.arguments()[1] == "next-song")
+        else if (msg.arguments()[1] == "stop-cd")
+            mafwrenderer->stop();
+        else if (msg.arguments()[1] == "next-song")
             mafwrenderer->next();
         else if (msg.arguments()[1] == "previous-song")
             mafwrenderer->previous();
